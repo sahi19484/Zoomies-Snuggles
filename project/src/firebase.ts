5546b541bd383bc640073c26ab5e7563c6116e3d
Sahil Mungara <163166255+sahi19484@users.noreply.github.com>
2026-01-20 12:24:37 +0530

    Update firebase.ts

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCbGTJjVvIRW6HdLVJAUWCtUqoDZbmYNGM",
   authDomain: "zoomies-snuggles.firebaseapp.com",
   projectId: "zoomies-snuggles",
-  storageBucket: "zoomies-snuggles.firebasestorage.app",
+  storageBucket: "zoomies-snuggles.appspot.com",   // ✅ fixed
   messagingSenderId: "43405536742",
   appId: "1:43405536742:web:4a83d4c5f2f687811e7a27",
   measurementId: "G-PGVYN4Y5FC"
@@ -19,5 +19,4 @@ const app = initializeApp(firebaseConfig);
 const db: Firestore = getFirestore(app);
 const auth: Auth = getAuth(app);
 
-// Export the services
 export { db, auth };
