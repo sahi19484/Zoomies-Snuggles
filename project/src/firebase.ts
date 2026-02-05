d9a8f71416a342b84bc1f8213cdc4d46fd81ae63
Sahil Mungara <163166255+sahi19484@users.noreply.github.com>
2026-02-05 09:02:28 +0530

    Update firebase.ts

--- project/src/firebase.ts
+++ project/src/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbGTJjVvIRW6HdLVJAUWCtUqoDZbmYNGM",
-  authDomain: "zoomies-snuggles.firebaseapp.com",
+  authDomain: "zoomiesnsneggles.vercel.app",
   projectId: "zoomies-snuggles",
   storageBucket: "zoomies-snuggles.appspot.com",   // ✅ fixed
   messagingSenderId: "43405536742",
